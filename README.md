748af6c0bad94528c6e1bf3cc08ffe7f12551cb9
Jezz Lucena <jezzlucena@gmail.com>
2020-12-01 11:34:48 -0800

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# web-portfolio
+# electro-entertainment-system
+
+A Sound Reactive Experiment using HTML5, CSS3, ES6, and lots of creative insight.
--- www/portfolio.php
+++ www/portfolio.php
@@ -2,15 +2,15 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-		<title>Portfolio - Jezz Lucena</title>
+		<title>Electro Entertainment System - by Jezz Lucena</title>
 		<link rel="shortcut icon" href="favicon.ico" />
-		<meta name="description" content="A collection of screencaps, videos, and live demos of UI/UX projects programmed by Jezz Lucena."/>
+		<meta name="description" content="A Sound Reactive Experiment using HTML5, CSS3, ES6, and lots of creative insight."/>
 
-		<meta property="og:title" content="Portfolio - Jezz Lucena"/>
+		<meta property="og:title" content="Electro Entertainment System - by Jezz Lucena"/>
 		<meta property="og:url" content="http://jezzlucena.com/"/>
 		<meta property="og:image" content="http://jezzlucena.com/resource/common/profile.jpg"/>
-		<meta property="og:site_name" content="Portfolio - Jezz Lucena"/>
-		<meta property="og:description" content="A collection of screencaps, videos, and live demos of UI/UX projects programmed by Jezz Lucena."/>
+		<meta property="og:site_name" content="Electro Entertainment System - by Jezz Lucena"/>
+		<meta property="og:description" content="A Sound Reactive Experiment using HTML5, CSS3, ES6, and lots of creative insight."/>
 
 		<?php include 'head_imports.php'; ?>
 	</head>
